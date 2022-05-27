b4235b84796576fe6b72c13d6e4d0544ef09ebad
Kelkias Emana <92637094+kelkias44@users.noreply.github.com>
2022-05-27 02:06:49 -0400

    Add files via upload

--- shared_preference/lib/main.dart
+++ shared_preference/lib/main.dart
@@ -1,28 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preference/data/data_source/local_data_source.dart';
+import 'package:moor/screen/note_list_page.dart';
+import 'package:provider/provider.dart';
 
-import 'presentation/pages/user_page.dart';
+import 'database/database.dart';
 
-void main()async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await LocalDataStorageImpl().init();
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'shared_preference',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Provider(
+      create: (context) => AppDatabase(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+          textTheme: const TextTheme(
+            headline5: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 24),
+            bodyText2: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 20),
+            bodyText1: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+                fontSize: 18),
+            subtitle2: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+                fontSize: 14),
+          ),
+        ),
+        home: const NoteListPage(),
       ),
-      home: HomePage(),
     );
   }
 }
-
-
-
 
