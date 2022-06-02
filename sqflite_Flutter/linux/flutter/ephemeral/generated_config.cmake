113eb86195a906a8e9772a9338a537c94a416568
Kelkias Emana <92637094+kelkias44@users.noreply.github.com>
2022-06-02 16:29:03 +0300

    Add files via upload

---
+++ sqflite_Flutter/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/kelkias/Adroid/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/kelkias/Documents/sqflite_flutter" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/kelkias/Adroid/flutter"
+  "PROJECT_DIR=/home/kelkias/Documents/sqflite_flutter"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/kelkias/Documents/sqflite_flutter/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/kelkias/Documents/sqflite_flutter/lib/main.dart"
+)
